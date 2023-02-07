e6b67aa50ed0ec7bd06058e3f1633a571c1531b0
TheMVProgrammer <franklin0415@hotmail.com>
2023-02-07 12:55:17 -0400

    Finishing Index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const http = require('http'); //Me permite crear un servidor, manejar las peticiones y crear las respuestas
+const { bodyParser } = require('./lib/bodyParser');
 
 let database = [];
 
@@ -8,10 +9,65 @@ function getTaskHandler(req, res) {
     res.end();
 }
 
-function createTaskHandler(req, res) {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.write('POST request received'); 
-    res.end();
+async function createTaskHandler(req, res) {
+    try {
+        await bodyParser(req);
+        database.push(req.body);
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.write(JSON.stringify(database)); 
+        res.end();
+    } catch (error) {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write('Invalid data'); 
+        res.end(); 
+    }
+}
+
+async function updateTaskHandler(req, res) {
+    try {
+        let { url } = req;
+    
+        let idQuery= url.split("?")[1];
+        let idKey = idQuery.split("=")[0];
+        let idValue = idQuery.split("=")[1];
+
+        if(idKey === 'id') {
+            await bodyParser(req);
+            database[idValue - 1] = req.body;
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.write(JSON.stringify(database)); 
+            res.end(); 
+        } 
+        else {
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.write('Invalid Request Query'); 
+            res.end(); 
+        }
+    } catch (error) {
+        res.writeHead(400, {'Content-Type': 'text/plain'});
+        res.write('Invalid body data was provided', error.message); 
+        res.end(); 
+    }
+}
+
+async function deleteTaskHandler(req, res) {
+    let { url } = req;
+
+    let idQuery= url.split("?")[1];
+    let idKey = idQuery.split("=")[0];
+    let idValue = idQuery.split("=")[1];   
+    
+    if(idKey === 'id') {
+        database.splice(idValue - 1, 1);
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write('Deleted successfully'); 
+        res.end(); 
+    }
+    else {
+        res.writeHead(400, {'Content-Type': 'text/plain'});
+        res.write('Invalid Query'); 
+        res.end(); 
+    }
 }
 
 const server = http.createServer((req, res) => {
@@ -30,15 +86,24 @@ const server = http.createServer((req, res) => {
             else if (url === '/tasks') {
                 getTaskHandler(req, res);
             }
+            break;
         case "POST":
             if (url === '/tasks') {
                 createTaskHandler(req, res);
             }
-        // case "PUT":
-        // case "DELETE":
-        // default: 
+            break;
+        case "PUT":
+            updateTaskHandler(req, res);
+            break;
+        case "DELETE":
+            deleteTaskHandler(req, res);
+            break;
+        default: 
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.write('404 NOT FOUND'); 
+            res.end();
     } 
-});
+}); 
 
 server.listen(3000);
 console.log('Server on port:', 3000);
